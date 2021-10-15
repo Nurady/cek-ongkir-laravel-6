b84c11d7d5197f2cf122c747e768fef6f49c893f
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-10-15 17:45:11 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::get('/', function () {
+    return redirect('home');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/store', 'HomeController@store')->name('store');
+Route::get('/api/province/{id}/cities', 'HomeController@getCities');
+
+Route::post('/api/cities', 'HomeController@searchCities');
